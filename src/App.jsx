9c3308a0567a9600336a41db1339a6c8c4a5b9c1
byungtoi-admin <pongdang123@gmail.com>
2024-10-07 13:47:54 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BootStrapButton from './BootstrapExample';
+import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import BoardPage from './pages/BoardPage';
+import ProfilePage from './pages/ProfilePage';
+import routes from './route/Routes';
+import HeaderNav from './components/HeaderNav';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <HeaderNav />
+        <Routes>
+          {routes.map((route, index) => {
+            return <Route key={route.path || index} path={route.path} element={route.element}></Route>;
+          })}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/route/Routes.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import HomePage from '../pages/HomePage';
+import BoardPage from '../pages/BoardPage';
+import ProfilePage from '../pages/ProfilePage';
+import PropsPage from '../pages/PropsPage';
+import ThreePage from '../pages/ThreePage';
+import CounterPage from '../pages/CounterPage';
+import InputPage from '../pages/InputPage';
+import GradePage from '../pages/GradePage';
+import UseRefPage from '../pages/UseRefPage';
+
+//React Routes, Route 사용시 배열로 관리하기
+const routes = [
+  {
+    path: '/',
+    element: <HomePage />,
+    title: 'Home',
+  },
+  {
+    path: '/board',
+    element: <BoardPage />,
+    title: 'Board',
+  },
+  {
+    path: '/profile',
+    element: <ProfilePage />,
+    title: 'profile',
+  },
+  {
+    path: '/props',
+    element: <PropsPage />,
+    title: '프롭스',
+  },
+  {
+    path: '/three',
+    element: <ThreePage />,
+    title: '조건부랜더링',
+  },
+  {
+    path: '/usestate',
+    element: <CounterPage />,
+    title: 'useState연습',
+  },
+  {
+    path: '/input',
+    element: <InputPage />,
+    title: 'input연습',
+  },
+  {
+    path: '/grade',
+    element: <GradePage />,
+    title: '성적표',
+  },
+  {
+    path: '/useref',
+    element: <UseRefPage />,
+    title: 'useRef 예제 1번',
+  },
+];
+
+export default routes;
